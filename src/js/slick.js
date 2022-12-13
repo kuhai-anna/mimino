966850defb8632f7b2813c5528712ca2f6146752
Aleksandr Slyvka <aleksandrslivka@gmail.com>
2022-12-13 23:08:46 +0100

    Додаю hero room, додаткові фото та стрілки для слайдерів

---
+++ src/js/slick.js
@@ -0,0 +1,19 @@
+$('.rooms').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+  // відстань між картинками
+  variableWidth: true,
+  // centerMode: true,
+  // appendArrows: $('.arrow'),
+  // prevArrow:
+  //   '<img class="slider-arrow slider-arrow__left" src="../images/arrow-left.svg">',
+  // nextArrow:
+  //   '<img class="slider-arrow slider-arrow__right" src="images/arrow-right.svg">',
+
+  // prevArrow: $('.btn-prev'),
+  // nextArrow: $('.btn-prev'),
+  // zIndex: 1,
+});
