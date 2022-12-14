eb695a217e11b5a5bace4153eac0ceb80aa63dfd
Roman Petrenko <rpetrenko0105@gmail.com>
2022-12-14 21:18:09 +0200

    добавляє ховери та переходи

--- src/js/script.js
+++ src/js/script.js
@@ -1,35 +1,25 @@
 // Наступний рядок це стандартний рядок для підключення JQuery
 $(document).ready(function () {
-  $('.slider').slick({
+  $('.slider-right').slick({
     arrows: true,
     dots: false,
     adaptiveHeight: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    speed: 300,
+    speed: 1000,
     infinite: true,
-    // Налаштування автопрокрутки
-    autoplay: false,
-    autoplaySpeed: 1000,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    pauseOnDotsHover: true,
-    // Налаштування Свопа на ДЕСКТОПІ
-    draggable: true,
-    // Налаштування Свопа на ПЛАНШЕТІ та ТЕЛЕФОНІ
-    swipe: true,
+
     // Величина прокрутки слайда, для того, щоб він спрацював
-    touchThreshold: 5,
+    touchThreshold: 20,
     // перелистування слайдів при постійному швидкому клацанні на кнопці.
-    waitForAnimate: false,
+    waitForAnimate: true,
     // Величина слайду відповідає ширині картинки у ньому
     variableWidth: true,
     //Параметр кількості рядків в ствобці (по 2-3 фото) в одому слайді.
     rows: 1,
-    // Кількість слайдів в області бачення
 
     //* Перекинути стрілки та крапки в потрібний нам div.arrows-wrap
-    appendArrows: $('.slider__arrows'),
+    appendArrows: $('.slider-right__arrows'),
   });
   $('.slider-left').slick({
     arrows: true,
@@ -37,22 +27,13 @@ $(document).ready(function () {
     adaptiveHeight: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    speed: 300,
+    speed: 1000,
     infinite: true,
-    // Налаштування автопрокрутки
-    autoplay: false,
-    autoplaySpeed: 1000,
-    pauseOnFocus: true,
-    pauseOnHover: true,
-    pauseOnDotsHover: true,
-    // Налаштування Свопа на ДЕСКТОПІ
-    draggable: true,
-    // Налаштування Свопа на ПЛАНШЕТІ та ТЕЛЕФОНІ
-    swipe: true,
+
     // Величина прокрутки слайда, для того, щоб він спрацював
-    touchThreshold: 5,
+    touchThreshold: 20,
     // перелистування слайдів при постійному швидкому клацанні на кнопці.
-    waitForAnimate: false,
+    waitForAnimate: true,
     // Величина слайду відповідає ширині картинки у ньому
     variableWidth: true,
     //Параметр кількості рядків в ствобці (по 2-3 фото) в одому слайді.
