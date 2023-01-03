429d90eac12dbfe99a227106cb7aa94d2c853016
Olena Savchenko <elena.volobuieva@gmail.com>
2023-01-03 21:16:49 +0300

    Задає стилі слайдеру при tablet і desktop

--- src/js/hotel-rooms-slider.js
+++ src/js/hotel-rooms-slider.js
@@ -1,14 +1,14 @@
 new Swiper(".hotel-rooms__slider", {
 	navigation: {
-		nextEl: '.swiper-btn-next',
-		prevEl: '.swiper-btn-prev'
+		nextEl: '.swiper-btn__next',
+		prevEl: '.swiper-btn__prev'
 	},
 	// Цикл слайдеру без кінця, по колу
 	loop: true,
 	// Кількість слайдів для одночасного показу
 	slidesPerView: 2,
 	// Відстань між слайдами
-	//  spaceBetween: 25,
+	spaceBetween: 40,
 
 
 });
