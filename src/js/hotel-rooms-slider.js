a86f0e48b2c367719c0c46c0c501598a63f48b7b
Olena Savchenko <elena.volobuieva@gmail.com>
2023-01-03 18:46:46 +0300

    Ініціалізує swiper-слайдер d hotel-rooms

---
+++ src/js/hotel-rooms-slider.js
@@ -0,0 +1,14 @@
+new Swiper(".hotel-rooms__slider", {
+	navigation: {
+		nextEl: '.swiper-btn-next',
+		prevEl: '.swiper-btn-prev'
+	},
+	// Цикл слайдеру без кінця, по колу
+	loop: true,
+	// Кількість слайдів для одночасного показу
+	slidesPerView: 2,
+	// Відстань між слайдами
+	//  spaceBetween: 25,
+
+
+});
